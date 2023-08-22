ef2c7915aa03d8ad557d56fc3dceb8681f98eea8
brijendrakrydv <brijendrakrydv@gmail.com>
2023-08-22 15:08:25 -0400

    Build

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -21,7 +21,7 @@ function About() {
             }}
           >
             <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              Know Who <strong className="purple">I'M</strong>
+              Know Who <strong className="purple">I'm</strong>
             </h1>
             <Aboutcard />
           </Col>
